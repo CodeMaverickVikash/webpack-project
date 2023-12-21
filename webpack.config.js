210054af8d67b99fb300ca41c5b1b9aff69d1b6c
vmaskhare <vmaskhare@asite.com>
2023-12-21 20:08:39 +0530

    configured webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,100 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const { merge } = require("webpack-merge");
 
 let config = {
-    entry: { main: './main.js' },
-    output: {
-        path: path.resolve(__dirname, 'bundles'),
-        filename: '[name].[contenthash].bundle.js',
-        clean: true
-    },
-    optimization: {
-        minimizer: [
-            new OptimizeCssAssetsPlugin(),
-            new TerserPlugin(),
-            new HtmlWebpackPlugin({
-                template: 'src/index.html',
-                minify: {
-                    removeAttributeQuotes: true,
-                    collapseWhitespace: true,
-                    removeComments: true
-                }
-            })
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].bundle.css'
-        })
+  entry: { main: "./main.js" },
+  output: {
+    path: path.resolve(__dirname, "bundles"),
+    filename: "[name].[contenthash].bundle.js",
+    clean: true,
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+      new HtmlWebpackPlugin({
+        template: "src/index.html",
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true,
+        },
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].[contenthash].bundle.css",
+      }),
     ],
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: ["html-loader"]
+  },
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: (url, resourcePath, context) => {
+              return path.relative(context, resourcePath);
             },
-            {
-                test: /\.(svg|png|jpg|gif)$/,
-                use: {
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].[ext]",
-                        outputPath: "imgs"
-                    }
-                }
-            },
-            // {
-            //     test: /\.scss$/,
-            //     use: [
-            //         // Creates `style` nodes from JS strings
-            //         "style-loader", // Inject styles into dOM
-            //         // Translates CSS into CommonJS
-            //         "css-loader", // Turn css into comomonJs
-            //         // Compiles Sass to CSS
-            //         "sass-loader", // Turn scss into css
-            //     ],
-            // },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader, //3. Extract css into files
-                    "css-loader", //2. Turns css into commonjs
-                    "sass-loader" //1. Turns sass into css
-                ]
-            }
+          },
+        },
+      },
+      // {
+      //     test: /\.scss$/,
+      //     use: [
+      //         // Creates `style` nodes from JS strings
+      //         "style-loader", // Inject styles into dOM
+      //         // Translates CSS into CommonJS
+      //         "css-loader", // Turn css into comomonJs
+      //         // Compiles Sass to CSS
+      //         "sass-loader", // Turn scss into css
+      //     ],
+      // },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, //3. Extract css into files
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader", //1. Turns sass into css
         ],
-    },
+      },
+    ],
+  },
 };
 
 module.exports = (env, argv) => {
-    config.mode = argv.mode;
-    if (config.mode === 'development') {
-        return merge(config, {
-            devtool: 'source-map',
-            plugins: [
-                new HtmlWebpackPlugin({
-                    template: 'src/index.html'
-                })
-            ]
-        });
-    }
-    return config;
+  config.mode = argv.mode;
+  if (config.mode === "development") {
+    return merge(config, {
+      devtool: "source-map",
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: "src/index.html",
+        }),
+        new MiniCssExtractPlugin({
+          filename: "[name].bundle.css",
+        }),
+      ],
+      devServer: {
+        static: path.join(__dirname, "dist"),
+        port: 3000,
+        open: true,
+      },
+      output: {
+        path: path.resolve(__dirname, "bundles"),
+        filename: "[name].bundle.js",
+        clean: true,
+      },
+    });
+  }
+  return config;
 };
